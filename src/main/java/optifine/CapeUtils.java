0f8af983caf208be926b4692a769b7f6f59dc412
Server24-7 <tim@server24-7.eu>
2019-06-13 09:59:37 +0200

    fix optifine capes

--- src/main/java/optifine/CapeUtils.java
+++ src/main/java/optifine/CapeUtils.java
@@ -4,13 +4,13 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.ThreadDownloadImageData;
 import net.minecraft.client.renderer.texture.ITextureObject;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
-import org.apache.commons.io.FilenameUtils;
 
 public class CapeUtils
 {
@@ -21,7 +21,7 @@ public class CapeUtils
         if (s != null && !s.isEmpty())
         {
             String s1 = "http://s.optifine.net/capes/" + s + ".png";
-            String s2 = FilenameUtils.getBaseName(s1);
+            String s2 = s;
             ResourceLocation resourcelocation = new ResourceLocation("capeof/" + s2);
             TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
             ITextureObject itextureobject = texturemanager.getTexture(resourcelocation);
